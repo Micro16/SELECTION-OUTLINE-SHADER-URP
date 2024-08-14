8bd044cf148cdb8bd23fc00906ce5c97f236d94f
Micro16 <33722176+Micro16@users.noreply.github.com>
2024-08-14 17:30:20 +0200

    Ajout d'une interface à la scène de test
    Ajout d'une interface à la scène de test pour faire avancer/reculer la caméra.

--- Assets/Scripts/ScrollMove.cs
+++ Assets/Scripts/ScrollMove.cs
@@ -28,9 +28,21 @@ public class ScrollMove : MonoBehaviour
 
 
     /// <summary>
-    /// Speed ​​along the Z axis
+    /// The multiplier applied to the mouse scroll value to adjust the movement along the Z axis
     /// </summary>
-    public float speed;
+    public float mouseScrollMultiplier;
+
+
+    /// <summary>
+    /// The speed of movement along the Z axis when a UI button is pressed
+    /// </summary>
+    public float uiButtonSpeed;
+
+
+    /// <summary>
+    /// The direction of movement given by the UI buttons
+    /// </summary>
+    private float direction;
     
     
     /// <summary>
@@ -40,6 +52,7 @@ public class ScrollMove : MonoBehaviour
     {
         Vector3 position = transform.position;
         position.z = zStart;
+        direction = 0.0f;
     }
 
 
@@ -48,11 +61,33 @@ public class ScrollMove : MonoBehaviour
     /// </summary>
     void Update()
     {
-        /// Move GameObject along Z axis according to mouse wheel input
+        /// Move GameObject along Z axis according to mouse wheel input and UI buttons
         Vector3 position = transform.position;
-        position.z += Input.mouseScrollDelta.y * speed;
+        if (direction == 0.0f)
+            position.z += Input.mouseScrollDelta.y * mouseScrollMultiplier;
+        else
+            position.z += direction * uiButtonSpeed * Time.deltaTime;
         position.z = Mathf.Clamp(position.z, zMin, zMax);
         transform.position = position;
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="dir"></param>
+    public void StartMove(float dir)
+    {
+        direction = dir;
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void StopMove()
+    {
+        direction = 0.0f;
     }
 
 }

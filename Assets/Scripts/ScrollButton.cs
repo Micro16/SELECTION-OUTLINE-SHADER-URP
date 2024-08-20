a2a5bec584e4b786b0c8d51365ab19dbcaa1473f
Micro16 <33722176+Micro16@users.noreply.github.com>
2024-08-20 16:47:31 +0200

    Ajout de commentaires
    Ajout de commentaires aux fichiers ScrollButton.cs et ScrollMove.cs

--- Assets/Scripts/ScrollButton.cs
+++ Assets/Scripts/ScrollButton.cs
@@ -4,23 +4,27 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+
+/// <summary>
+/// Behavior that defines the logic of the buttons to move the camera forward/backward
+/// </summary>
 public class ScrollButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
 
     /// <summary>
-    /// 
+    /// Defines an enumeration to indicate the direction of movement along the Z axis for the button to which this script is attached
     /// </summary>
     public enum Direction { Forward, Backward };
 
 
     /// <summary>
-    /// 
+    /// The direction variable (see above) to be set in the inspector
     /// </summary>
     public Direction direction;
 
 
     /// <summary>
-    /// 
+    /// The ScrollMove component of the main camera
     /// </summary>
     private ScrollMove scrollCamera;
     
@@ -46,21 +50,21 @@ public class ScrollButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandle
 
 
     /// <summary>
-    /// 
+    /// Evaluate current state and transition to pressed state
     /// </summary>
-    /// <param name="eventData"></param>
+    /// <param name="eventData">The EventData usually sent by the EventSystem</param>
     public void OnPointerDown(PointerEventData eventData)
     {
-        /// Start moving camera with the 
+        /// Start moving camera with the right direction
         if (scrollCamera != null)
             scrollCamera.StartMove(direction == Direction.Forward ? 1.0f : -1.0f);
     }
 
 
     /// <summary>
-    /// 
+    /// Evaluate current state and transition to released state
     /// </summary>
-    /// <param name="eventData"></param>
+    /// <param name="eventData">The EventData usually sent by the EventSystem</param>
     public void OnPointerUp(PointerEventData eventData)
     {
         if (scrollCamera != null)
